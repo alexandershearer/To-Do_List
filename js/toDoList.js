931e72ac6c4b43d9c00419c67b956b9d46eb75ce
Alex Shearer <alex.shearer23@gmail.com>
2019-09-24 10:45:55 -0500

    Added js and css to item elements

---
+++ js/toDoList.js
@@ -0,0 +1,36 @@
+var submit = document.getElementById("submit");
+var input = document.getElementById("userInput");
+var ul = document.querySelector("ul");
+
+
+function createListElement() {
+    var li = document.createElement("li");
+    li.innerHTML = input.value;
+    ul.appendChild(li);
+
+    input.value = "";
+
+    function markDone() {
+        li.classList.toggle("done");
+    }
+
+    li.addEventListener("click", markDone);
+}
+
+
+function createListItemSubmitButton() {
+    if (input.value.length > 0) {
+        createListElement();
+    }
+
+}
+
+function createListItemEnterKey(event) {
+    if (input.value.length > 0 && event.keyCode == 13) {
+        createListElement();
+    }
+}
+
+submit.addEventListener("click", createListItemSubmitButton);
+
+input.addEventListener("keypress", createListItemEnterKey);
